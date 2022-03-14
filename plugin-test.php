4a4b5b6217258b0d5d001d50544a0fbf06964091
VitaliyNosov <webdevelorestudio2017@gmail.com>
2022-03-14 18:50:27 +0200

    first commit

---
+++ plugin-test.php
@@ -0,0 +1,115 @@
+<?php
+
+/**
+ * Plugin Name:       Plugin Test
+ * Plugin URI:        https://gifted-fermat-f7e371.netlify.app/
+ * Description:       Plugin test for company LANARS
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.4
+ * Author:            Vitaliy Nosov
+ * Author URI:        https://www.linkedin.com/in/vitaliy-nosov-5543a8173/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       plugin-test
+ * Domain Path:       /languages
+ */
+
+
+// Basic plugin protection:
+
+if( !defined('ABSPATH')){
+    die;
+}
+
+// An alternative to this notation, you can use any:
+
+// defined('ABSPATH') || exit;
+
+// Display function in the admin panel menu
+
+function plugin_test_show_item(){
+    add_menu_page(
+        esc_html__( 'Welcome to plugin page', 'plugin-test'),
+        esc_html__('Plugin Test', 'plugin-test'),
+        'manage_options',
+        'plugin-test-options',
+        'plugin_test_content',
+        'dashicons-admin-plugins',
+        6
+    );
+}
+
+add_action('admin_menu','plugin_test_show_item');
+
+
+// The function of displaying the plugin page in the admin panel
+
+function plugin_test_content(){
+    echo '
+    <div class="content">
+        <div class="border">
+        <div class="info__text">
+            <p>This plugin is a test development. Check the competence of the developer for the company:<img src="https://lanars.com/static/logo_icon.svg"></p> 
+            <p>To make sure the plugin works, copy the shortcode and paste it on the page in the editor.</p>
+        </div>
+        </div>
+            <div class="text">
+                <span>Copy shortcode:</span>
+            <div class="text__copy_link">[post-test-shortcode]</div>
+        </div>
+            <div class="copy__link_mess">Copied to clipboard</div>
+        </div> ';
+    
+}
+
+// Style and Script Registration Function
+
+function plugin_register_assets(){
+    wp_register_style('plugin_test_styles', plugins_url('assets/css/plugin-style.css', __FILE__));
+    wp_register_script('plugin_test_jquery', plugins_url('assets/js/jquery.min.js', __FILE__));
+    wp_register_script('plugin_test_scripts', plugins_url('assets/js/admin.js', __FILE__));
+}
+
+add_action('admin_enqueue_scripts','plugin_register_assets');
+
+// The function of connecting styles and scripts
+
+function plugin_load_assets($hook){
+    if($hook != 'toplevel_page_plugin-test-options'){
+        return;
+    }
+    wp_enqueue_style('plugin_test_styles');
+    wp_enqueue_script('plugin_test_scripts');
+
+    // If you need to use styles or scripts that are already built into WordPress: 
+
+    // wp_enqueue_script('jquery-ui-tabs');
+
+
+}
+
+add_action('admin_enqueue_scripts','plugin_load_assets');
+
+// Connecting styles for display on the frontend
+
+function plugin_test_scripts() {
+	wp_enqueue_style( 'style', plugins_url('assets/css/plugin-style.css', __FILE__) );
+	wp_register_script('plugin_test_jquery', plugins_url('assets/js/jquery.min.js', __FILE__));
+    wp_register_script('plugin_test_scripts', plugins_url('assets/js/admin.js', __FILE__));	
+}
+
+add_action( 'wp_enqueue_scripts', 'plugin_test_scripts' );
+
+// We connect our shortcode:
+
+require_once( dirname(__FILE__) . '/post-test-shortcode.php');
+
+
+
+
+
+
+
+
---
+++ post-test-shortcode.php
@@ -0,0 +1,103 @@
+<?php 
+
+// Tasks:
+
+// ----------------------------------------------------
+
+// Develop custom plugin that serves a shortcode for displaying the post with the most
+// comments - the title, short description, picture, publication date and the actual
+// number of comments must be displayed.
+
+// Respectively:
+// 1) deploy wordpress,
+// 2) install a plugin that will serve such a shortcode,
+// 3) make some page the main (not the list of posts, as by default)
+// 4) insert this shortcode into this page, insert 1 paragraph of lorem ipsum text above it
+
+// Additional tasks:
+// 1) Do not display posts 0 comments - if nothing was found, do not display anything
+// 2) Display a thumbnail of the publication above the title, registering the size of
+// 200x100 and uploading the images so that they have this size
+// 3) In the widget, when you hover over the picture, make 2 effects:
+// - overlay appears on top of the picture with a gradient of light gray to light turquoise
+// - the picture is enlarged by 20% (it does not go beyond the original dimensions) and
+// rotated 30 degrees clockwise
+// - all this happens smoothly for 300 ms
+
+// Supporting materials for the test task:
+// https://developer.wordpress.org/plugins/intro/
+
+// ----------------------------------------------------
+
+
+// Post test shortcode 
+
+	function post_test_shortcode(){
+		ob_start();
+		?>
+			<!-- Development shortcode -->
+
+			
+		<ul class="most__commented">
+			<?php 
+
+					// Filter 0 comment
+
+						function my_has_comments_filter( $where ) {
+							$where .= ' AND comment_count > 0 ';
+							return $where;
+						}
+						add_filter( 'posts_where', 'my_has_comments_filter' );
+							// Run WP_Query
+							// change posts_per_page value to limit the number of posts
+						$query = new WP_Query('orderby=comment_count&posts_per_page=10');		
+							// Don't filter future queries.
+						remove_filter( 'posts_where', 'my_has_comments_filter' );
+
+					// Filter
+
+				//Post begin loop
+				while ($query->have_posts()) : $query->the_post(); ?>
+
+			<li class='post__body'>
+				<a class="post__image" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail(array(200,100)); ?>
+					<div class="cover__bg"></div>
+				</a>	
+					</br>
+    			<a class="post__header" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+					</br>
+    			<a class="post__info" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_excerpt($post->ID); ?></a>
+					</br>
+    			<a class="post__autor" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_author(); ?></a>
+    				</br>
+    			<a class="post__data" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_time(' Y-j-m '); ?></a>	
+					</br>		
+				<span class="wpb-comment-count"><?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?></span>
+			</li>
+				</br>
+
+				<?php endwhile; 
+					// end loop
+			?>
+		</ul>
+			<?php
+  
+				// Turn off output buffering
+ 			$output = ob_get_clean(); 
+  
+				//Return output 
+			return $output; 
+
+			?>
+		}
+		
+			<!-- Development shortcode -->
+		<?php
+	
+		return ob_get_clean();
+
+
+	}
+	add_shortcode('post-test-shortcode', 'post_test_shortcode');
+
+
